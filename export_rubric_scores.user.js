c333a2e6842f51ec41f0d64a4d1ecd22dbd55d7c
unity-hallie <157060474+unity-hallie@users.noreply.github.com>
2024-01-29 14:32:31 -0500

    Release Candidate. Added support for quizzes. Data rows are now outputting correctly. Instructor names added. Submission state added. Added ability to run for a single assignment on page.

--- export_rubric_scores.user.js
+++ export_rubric_scores.user.js
@@ -5,16 +5,15 @@
 // @match        https://*/courses/*/gradebook/speed_grader?*
 // @grant        none
 // @run-at       document-idle
-// @version      0.4
+// @version      0.5
 // ==/UserScript==
 
 /* globals $ */
 // wait until the window jQuery is loaded
-const debug = false;
 let header = [
-    'Term','Class','Section','Student Name','Student Id',
-    'Week Number', 'Assignment Type','Assignment Number', 'Assignment Id', 'Assignment Title',
-    'Rubric Id', 'Rubric Line', 'Line Name', 'Line Score','Line Max Score', 'Assignment Score', 'Total Points Possible'
+    'Term', 'Instructor', 'Class','Section','Student Name','Student Id',
+    'Week Number', 'Assignment Type','Assignment Number', 'Assignment Id', 'Assignment Title', 'Submission Status',
+    'Rubric Id', 'Rubric Line', 'Line Name', 'Score','Max Score',
 ].join(',');
 header += '\n';
 
@@ -69,7 +68,7 @@ async function getRemainingPagesAsync(url, listSoFar) {
 // escape commas and quotes for CSV formatting
 function csvEncode(string) {
 
-    if (typeof(string) === 'undefined') {
+    if (typeof(string) === 'undefined' || string === null || string === 'null') {
         return '';
     }
     string = String(string);
@@ -78,7 +77,7 @@ function csvEncode(string) {
         string = string.replace(/(")/g,'"$1');
         string = string.replace(/\s*\n\s*/g,' ');
     }
-    return string;
+    return `"${string}"`;
 }
 
 function showError(event) {
@@ -91,11 +90,13 @@ function getItemInModule(contentItem, module) {
     let type= 'Assignment';
     if( contentItem.hasOwnProperty('discussion_topic')) {
         type = 'Discussion';
-        contentId = contentItem.discussion_topic.id
+        contentId = contentItem.discussion_topic.id;
     }
-    if( 'online_quiz' in contentItem.submission_types) {
+    if(contentItem.hasOwnProperty('quiz_id')) {
         type = 'Quiz';
+        contentId = contentItem.quiz_id;
     }
+    
     console.log(type);
     let count = 1;
     for (let item of module.items){
@@ -109,14 +110,69 @@ function getItemInModule(contentItem, module) {
         count++;
     }
 }
+
+async function exportData(singleAssignment=false, saveText) {
+
+    try {
+        popUp("Exporting scores, please wait...");
+        window.addEventListener("error", showError);
+
+       // Get some initial data from the current URL
+        const urlParams = window.location.href.split('?')[1].split('&');
+        const courseId = window.location.href.split('/')[4];
+
+        let courseResponse = await fetch(`/api/v1/courses/${courseId}?include=term`)
+        let course = await courseResponse.json()
+
+        const assignId = urlParams.find(i => i.split('=')[0] === "assignment_id").split('=')[1];
+        let assignRequest = await fetch(`/api/v1/courses/${courseId}/assignments/${assignId}`);
+        let assignment = await assignRequest.json();
+        let baseSubmissionsUrl = singleAssignment? `/api/v1/courses/${courseId}/assignments/${assignId}/submissions` : `/api/v1/courses/${courseId}/students/submissions`;
+        let userSubmissions = await getAllPagesAsync(`${baseSubmissionsUrl}?student_ids=all&per_page=100&include[]=rubric_assessment&include[]=assignment&include[]=user&grouped=true`);
+
+        let accounts = await getAllPagesAsync(`/api/v1/accounts/${course.account_id}`);
+        let account = accounts[0];
+
+        let rootAccountId = account.root_account_id;
+        let instructors = await getAllPagesAsync(`/api/v1/courses/${courseId}/users?enrollment_type=teacher`);
+        let modules = await getAllPagesAsync(`/api/v1/courses/${courseId}/modules?include[]=items&include[]=content_details`);
+        let enrollments = await getAllPagesAsync(`/api/v1/courses/${courseId}/enrollments?per_page=100`);
+
+        let response = await fetch(`/api/v1/accounts/${rootAccountId}/terms/${course.enrollment_term_id}`);
+        let term = await response.json();
+
+        let csvRows = [header];
+        for(let enrollment of enrollments) {
+            let assignmentId = singleAssignment? assignId : null;
+            let out_rows = await getRows({
+                enrollment, modules, userSubmissions, term, course, assignmentId, instructors,
+            });
+            csvRows = csvRows.concat(out_rows);
+        }
+        popClose();
+        console.log(csvRows);
+        let filename = singleAssignment? assignment.name : course.course_code;
+        saveText(csvRows, `Rubric Scores ${filename.replace(/[^a-zA-Z 0-9]+/g, '')}.csv`);
+        saveText([JSON.stringify(userSubmissions, null, 2)], `User Submissions ${filename.replace(/[^a-zA-Z 0-9]+/g, '')}.json`);
+
+        window.removeEventListener("error", showError);
+
+    } catch(e) {
+        popClose();
+        popUp(`ERROR ${e} while retrieving assignment data from Canvas. Please refresh and try again.`, null);
+        window.removeEventListener("error", showError);
+        throw(e);
+    }
+}
+
 function getModuleInfo(contentItem, modules) {
     const regex = /week (\d+)/i;
-    let weekCounter = 0;
+
     for (let module of modules) {
         let match = module.name.match(regex);
         let weekNumber = Number(match? match[1] : null);
         if(!weekNumber) {
-            for(let moduleItem in module.items) {
+            for(let moduleItem of module.items) {
                 if(!moduleItem.hasOwnProperty('title')) {
                     continue;
                 }
@@ -143,74 +199,98 @@ function getModuleInfo(contentItem, modules) {
 /**
  *
  * @param {object} course
+ * The course
  * @param {object} enrollment
+ * The enrollment of the user to generate rows for
  * @param {array} modules
- * @param {array} quizzes
+ * All modules in the course
+ * @param {int} assignmentId
+ * The ID of the assignment to retrieve data for, if any
+ * @param {array} instructors
+ * The instructors of the course
  * @param {array} userSubmissions
+ * an object containing an array of user submissions { user_id, submissions: []}
+ * OR just an array of all users submissions for a single assignment, if assignmentId is specified
  * @param {object} term
+ * The term
  * @returns {Promise<string[]>}
  */
-async function getEnrollmentRows({ course, enrollment, modules, quizzes, userSubmissions,
-                                     rubrics, term }){
+async function getRows({
+        course,
+        enrollment,
+        modules,
+        userSubmissions,
+        instructors,
+        term
+    })
+{
+
     let { user } = enrollment;
     let singleUserSubmissions = userSubmissions.filter(a => a.user_id === user.id);
 
-    //Lets not actually do this if we can't find the user's submissions.
+    const {course_code} = course;
+    let section = course_code.match(/-\s*(\d+)$/);
+    if (section) { section = section[1]; }
+
+    let instructorName
+
+    if(instructors.length > 1) {
+        instructorName = instructors.map(a => a.name).join(',')
+    } else {
+        instructorName = instructors[0].name;
+    }
+    // Let's not actually do this if we can't find the user's submissions.
     if (singleUserSubmissions.length === 0) {
         return [];
     }
-    let submissions = singleUserSubmissions[0].submissions;
-    let skip = false;
-    let hide_points, free_form_criterion_comments = false;
-    const out = [];
 
+    let submissions;
+    let entry = singleUserSubmissions[0];
+    if (entry.hasOwnProperty('submissions')) {
+        submissions = entry.submissions;
+    } else {
+        submissions = [entry];
+    }
+    let assignmentsById = {};
+    for(let submission of submissions) {
+        let  { assignment } = submission;
+        if(!assignment.id in Object.keys(assignmentsById)){
+            assignmentsById[assignment.id] = assignment;
+        }
+    }
+
+    const rows = [];
+    let baseRow = [
+        term.name,
+        instructorName,
+        course.course_code,
+        section,
+
+    ]
 
     for (let submission of submissions) {
         let { assignment } = submission;
-        const {course_code} = course;
-        let section = course_code.match(/-\s*(\d+)$/);
-        if (section) {
-            section = section[1];
-        }
 
-        let rubricCriteria = [];
+
         let rubricSettings;
-        if (assignment.hasOwnProperty('rubric')) {
-            rubricCriteria = assignment.rubric;
-        }
 
         if (assignment.hasOwnProperty('rubric_settings')) {
             rubricSettings = assignment.rubric_settings;
         }
-
-        // Fill out the csv header and map criterion ids to sort index
-        // Also create an object that maps criterion ids to an object mapping rating ids to descriptions
-        let critOrder = {};
-        let critRatingDescs = {};
-        let critsById = {};
-
-        for (let critIndex in rubricCriteria){
-            let rubricCriterion = rubricCriteria[critIndex];
-            critOrder[rubricCriterion.id] = critIndex;
-            critRatingDescs[rubricCriterion.id] = {};
-            critsById[rubricCriterion.id] = rubricCriterion;
-
-            for(let rating of rubricCriterion.ratings) {
-                critRatingDescs[rubricCriterion.id][rating.id] = rating.description;
-            }
-        }
-
+        let { critOrder, critRatingDescs, critsById } = getCriteriaInfo(assignment);
 
         course_code.replace(/^.*_?(\[A-Za-z]{4}\d{3}).*$/, /\1\2/)
         let { weekNumber, numberInModule, type } = getModuleInfo(assignment, modules);
-        console.log("number in module", numberInModule);
+        let { rubric_assessment: rubricAssessment } = submission;
+        let { rubric_settings } = assignment;
+        let rubricId = typeof(rubric_settings) !== 'undefined' && rubric_settings.hasOwnProperty('id')?
+            rubric_settings.id : 'No Rubric Settings';
 
         if (user) {
             // Add criteria scores and ratings
             // Need to turn rubric_assessment object into an array
             let critAssessments = []
             let critIds = []
-            let { rubric_assessment: rubricAssessment } = submission;
 
             if (rubricAssessment !== null) {
                 for (let critKey in rubricAssessment) {
@@ -218,7 +298,7 @@ async function getEnrollmentRows({ course, enrollment, modules, quizzes, userSub
                     let crit = {
                         'id': critKey,
                         'points': critValue.points,
-                        'rating': 'N/A'
+                        'rating': null
                     }
                     if(critValue.rating_id) {
                         if(critKey in critRatingDescs) {
@@ -231,10 +311,30 @@ async function getEnrollmentRows({ course, enrollment, modules, quizzes, userSub
                     critIds.push(critKey);
                 }
             }
+            let submissionBaseRow = baseRow.concat([
+                user.name,
+                user.sis_user_id,
+                weekNumber,
+                type,
+                numberInModule,
+                assignment.id,
+                assignment.name,
+                submission.workflow_state,
+            ]);
+
+            rows.push(submissionBaseRow.concat([
+                rubricId,
+                'Total',
+                'Total',
+                submission.grade,
+                assignment.points_possible
+
+            ]))
+
             // Check for any criteria entries that might be missing; set them to null
             for (let critKey in critOrder) {
                 if (!critIds.includes(critKey)) {
-                    critAssessments.push({'id': critKey, 'points': 'N/A', 'rating': 'N/A'});
+                    critAssessments.push({'id': critKey, 'points': null, 'rating': null});
                 }
             }
             // Sort into same order as column order
@@ -242,39 +342,57 @@ async function getEnrollmentRows({ course, enrollment, modules, quizzes, userSub
                 return critOrder[a.id] - critOrder[b.id];
             });
 
-
             for(let critIndex in critAssessments) {
                 let critAssessment = critAssessments[critIndex];
                 let criterion = critsById[critAssessment.id];
-                let { rubric_settings } = assignment;
-                let rubricLine = critIndex;
-                console.log(assignment);
-                let rubricName = rubric_settings ? rubric_settings.title : null;
-// let header = [
-//     'Term','Class','Section','Student Name','Student Id',
-//     'Week Number', 'Assignment Type','Assignment Number', 'Assignment Id', 'Assignment Title',
-//     'Rubric Id', 'Rubric Line', 'Line Name', 'Line Score','Line Max Score'
-// ].join(',');
 
-
-                let row = [
-                    term.name, course.course_code, section, user.name, user.sis_user_id,
-                    weekNumber, type, numberInModule, assignment.id, assignment.name,
-                    rubricCriteria.id, rubricLine, criterion.description,
-                    critAssessment.points, criterion.points, submission.grade, assignment.points_possible
-                ];
-                console.log(row);
-                row = row.map( item => csvEncode(item));
-                let row_string = row.join(',') + '\n';
-                out.push(row_string);
+                rows.push(submissionBaseRow.concat([
+                    criterion.id,
+                    critIndex,
+                    criterion.description,
+                    critAssessment.points,
+                    criterion.points
+                ]));
             }
         }
 
     }
+    let out = [];
+    for (let row of rows) {
+        let row_string = row.map( item => csvEncode(item)).join(',') + '\n';
+        out.push(row_string);
+    }
     return out;
-
 }
 
+/**
+ * Fill out the csv header and map criterion ids to sort index
+ * Also create an object that maps criterion ids to an object mapping rating ids to descriptions
+ * @param assignment
+ * The assignment from canvas api
+ * @returns {{critRatingDescs: *[], critsById: *[], critOrder: *[]}}
+ */
+function getCriteriaInfo(assignment) {
+    if (!assignment || !assignment.hasOwnProperty('rubric')){
+        return {critsById: [], critRatingDescs: [], critOrder: []}
+    }
+    let rubricCriteria = assignment.rubric;
+
+    let critOrder = [];
+    let critRatingDescs = [];
+    let critsById = [];
+        for (let critIndex in rubricCriteria){
+        let rubricCriterion = rubricCriteria[critIndex];
+        critOrder[rubricCriterion.id] = critIndex;
+        critRatingDescs[rubricCriterion.id] = {};
+        critsById[rubricCriterion.id] = rubricCriterion;
+
+        for(let rating of rubricCriterion.ratings) {
+            critRatingDescs[rubricCriterion.id][rating.id] = rating.description;
+        }
+    }
+    return { critOrder, critRatingDescs, critsById }
+}
 
 defer(function() {
     'use strict';
@@ -295,56 +413,15 @@ defer(function() {
 
     $("body").append($('<div id="export_rubric_dialog" title="Export Rubric Scores"></div>'));
     // Only add the export button if a rubric is appearing
-    if ($('#rubric_summary_holder').length > 0) {
-        $('#gradebook_header div.statsMetric').append('<button type="button" class="Button" id="export_all_rubric_btn">Export All</button>');
-        $('#export_all_rubric_btn').click(async function() {
-            try{
-                popUp("Exporting scores, please wait...");
-                window.addEventListener("error", showError);
+    let el = $('#gradebook_header div.statsMetric');
+    el.append('<button type="button" class="Button" id="export_one_rubric_btn">Export Assignment</button>');
+    el.append('<button type="button" class="Button" id="export_all_rubric_btn">Export All</button>');
 
-               // Get some initial data from the current URL
-                const urlParams = window.location.href.split('?')[1].split('&');
-                const courseId = window.location.href.split('/')[4];
+    $('#export_one_rubric_btn').click(async function() {
+        await exportData(true, saveText);
+    });
+    $('#export_all_rubric_btn').click(async function() {
+        await exportData(false, saveText);
+    });
 
-                let courseResponse = await fetch(`/api/v1/courses/${courseId}?include=term`)
-                let course = await courseResponse.json()
-
-                let assignments = await getAllPagesAsync(`/api/v1/courses/${courseId}/assignments?per_page=100`);
-                let userSubmissions = await getAllPagesAsync(`/api/v1/courses/${courseId}/students/submissions?student_ids=all&per_page=100&include[]=rubric_assessment&include[]=assignment&include[]=user&grouped=true`);
-                //let quizzes = await getAllPagesAsync(`/api/v1/courses/${courseId}/quizzes`);
-
-                let accounts = await getAllPagesAsync(`/api/v1/accounts/${course.account_id}`);
-                let account = accounts[0];
-
-                let rootAccountId = account.root_account_id;
-                let enrollments = await getAllPagesAsync(`/api/v1/courses/${courseId}/enrollments?per_page=100`);
-                let modules = await getAllPagesAsync(`/api/v1/courses/${courseId}/modules?include[]=items&include[]=content_details`);
-
-                let response = await fetch(`/api/v1/accounts/${rootAccountId}/terms/${course.enrollment_term_id}`);
-                let term = await response.json();
-
-
-                let rubrics = await getAllPagesAsync(`/api/v1/courses/${courseId}/rubrics?per_page=100`);
-
-                let csvRows = [header];
-                for(let enrollment of enrollments) {
-                    let out_rows = await getEnrollmentRows({
-                        enrollment, modules, assignments, userSubmissions, term, rubrics, course,
-                    });
-                    csvRows = csvRows.concat(out_rows);
-                }
-                popClose();
-                console.log(csvRows);
-                saveText(csvRows, `Rubric Scores ${course.course_code.replace(/[^a-zA-Z 0-9]+/g, '')}.csv`);
-                saveText([JSON.stringify(userSubmissions)], `User Submissions ${course.course_code.replace(/[^a-zA-Z 0-9]+/g, '')}.json`);
-
-                window.removeEventListener("error", showError);
-            } catch(e) {
-                popClose();
-                popUp(`ERROR ${e} while retrieving assignment data from Canvas. Please refresh and try again.`, null);
-                window.removeEventListener("error", showError);
-                throw(e);
-            }
-        });
-    }
 });
